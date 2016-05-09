1bfde9636bda8b41c25c10c4107f016f340b3f8e
LCAdmin14 <lcadmin14@LCAdmin14s-MacBook-Air.local>
2016-05-09 09:03:08 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// var lion = "Rawr";
+// var num = 18;
+// var oldMan =
+
+
+if (20 < 30) {
+	console.log('rrhrhtyk');
+}
+var candy = ["twix", "reesies"];
+alert(candy[0]);
+var movie = ["batman", "superman"];
+document.write(movie[0]);
+
+for (var i = 0; i <= candy.length - 1; i++) {
+	console.log(i + " " + candy[i])
+}
+
+
